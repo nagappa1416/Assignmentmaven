d80d4c99202195655f04cd90325af8fb01ed118e
nagappa <naguparu1419@gmail.com>
2022-09-15 01:59:33 +0530

    first commit

---
+++ src/test/java/com/example/Assignmentsample/AssignmentsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Assignmentsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
